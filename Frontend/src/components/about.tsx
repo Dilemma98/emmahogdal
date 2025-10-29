d2d9e1714f6cf9081981f3524e4c0338dd3d5897
Emma Högdal <emmaah98@outlook.com>
2025-10-29 08:19:56 +0100

    added tech stack

--- Frontend/src/components/about.tsx
+++ Frontend/src/components/about.tsx
@@ -1,19 +1,33 @@
 const About = () => {
-    return (
-        <div>
-            <h2>About me</h2>
-            <div className="info">
-                <p>My name is Emma, a web development student since 
-                    August 2024, focused on mastering both frontend 
-                    and backend technologies. I am passionate about 
-                    improving user experience and accessibility, and 
-                    I enjoy solving problems to create clean, functional
-                    websites. My goal is to continue growing as a developer 
-                    and contribute to innovative digital projects. I am 
-                    committed to continuous learning and expanding my skills.</p>
-            </div>
+  return (
+    <div>
+      <h2 className="aboutHeader">About me</h2>
+      <div className="info">
+        <p>
+          My name is Emma, and I’ve been studying web development since August
+          2024, focusing on mastering both frontend and backend technologies.
+          I’m passionate about creating clean, accessible, and user-friendly
+          websites, and I enjoy solving problems through thoughtful design and
+          efficient code. My goal is to continue growing as a developer and
+          contribute to innovative digital projects while continuously expanding
+          my skills and knowledge.
+        </p>
+        <div className="techStack">
+          <h3>Tech stack</h3>
+          <div className="tech-bubbles">
+            <span>C#</span>
+            <span>JavaScript</span>
+            <span>TypeScript</span>
+            <span>React</span>
+            <span>Vue</span>
+            <span>Angular</span>
+            <span>Tailwind CSS</span>
+            <span>Bootstrap</span>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default About;
