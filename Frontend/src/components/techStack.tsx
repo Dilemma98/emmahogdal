8409afd97360450ff5ccbc861c36b3fb3a017f74
Emma Högdal <emmaah98@outlook.com>
2025-11-12 19:39:14 +0100

    Update techStack.tsx

--- Frontend/src/components/techStack.tsx
+++ Frontend/src/components/techStack.tsx
@@ -11,8 +11,8 @@ const TechStack = () => {
             <span>JavaScript</span>
             <span>TypeScript</span>
             <span>React</span>
+            <span>React-Native</span>
             <span>Vue</span>
-            <span>Angular</span>
             <span>Tailwind CSS</span>
             <span>Bootstrap</span>
             <span>Azure</span>
