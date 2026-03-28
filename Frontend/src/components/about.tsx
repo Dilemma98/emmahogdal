5a8073e1c113b8a755de251b2136c5fddd8314d3
Emma Högdal <emmaah98@outlook.com>
2026-03-28 06:09:11 +0100

    added new project

--- Frontend/src/components/about.tsx
+++ Frontend/src/components/about.tsx
@@ -4,13 +4,18 @@ const About = () => {
       <h2 className="aboutHeader">About me</h2>
       <div className="info">
         <p>
-          My name is Emma, and I’ve been studying web development since August
-          2024, focusing on mastering both frontend and backend technologies.
-          I’m passionate about creating clean, accessible, and user-friendly
-          websites, and I enjoy solving problems through thoughtful design and
-          efficient code. My goal is to continue growing as a developer and
-          contribute to innovative digital projects while continuously expanding
-          my skills and knowledge.
+          My name is Emma, and I am currently studying a full-stack web
+          development program, which I have been pursuing since August 2024.
+          While my education covers both frontend and backend technologies, I am
+          particularly drawn to frontend development, where I enjoy creating
+          clean, accessible, and user-friendly interfaces. I also enjoy working
+          with client-based applications, as it allows me to build interactive
+          and engaging user experiences. I am continuously working on improving
+          how I structure my code and developing more efficient and thoughtful
+          solutions. I enjoy problem-solving and strive to write well-organized
+          and maintainable code. My goal is to continue growing as a developer 
+          and to expand my skills across the full stack and contributing to 
+          innovative and meaningful digital projects.
         </p>
       </div>
     </div>
