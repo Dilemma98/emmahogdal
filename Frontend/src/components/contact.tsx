ded3d8333d531a6d558eee33324a59743dda31b3
Emma Högdal <emmaah98@outlook.com>
2026-03-31 09:29:25 +0200

    updated mailto: adress

--- Frontend/src/components/contact.tsx
+++ Frontend/src/components/contact.tsx
@@ -5,7 +5,7 @@ const ContactMe = () => {
         <div className="contactMe">
             <h2>Contact me</h2>
             <div className="links">
-                <a href="mailto:emmaah98@outlook.com" target="_blank"><i className="fa-solid fa-envelope"></i></a>
+                <a href="mailto:emma.h98@outlook.com" target="_blank"><i className="fa-solid fa-envelope"></i></a>
                 <a href="https://www.linkedin.com/in/emma-h%C3%B6gdal-07167b326/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
                 <a href="https://github.com/Dilemma98" target="_blank"><i className="fa-brands fa-github"></i></a>
             </div>
