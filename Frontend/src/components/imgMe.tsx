28be95a670136c2ef3a5d6be62315200ea8f4ffa
Emma Högdal <emmaah98@outlook.com>
2026-04-09 15:40:59 +0200

    added text under img

--- Frontend/src/components/imgMe.tsx
+++ Frontend/src/components/imgMe.tsx
@@ -2,10 +2,23 @@ import imgUrl from "../assets/images/imgMe.jpg";
 import "../stylesheets/imgMe.css";
 
 const ImgMe = () => {
-  return(
-    <div style={{marginTop: "6em"}}>
-     <img className="imgMe" src={imgUrl} alt="Bild på Emma" />
-     </div>
+  return (
+    <div style={{ marginTop: "6em" }}>
+      <img className="imgMe" src={imgUrl} alt="Bild på Emma" />
+      <p
+        style={{
+          fontStyle: "italic",
+          opacity: 0.7,
+          fontSize: "0.85rem",
+          textAlign: "center",
+          lineHeight: "1.5"
+        }}
+      >
+        v1.0 released ’98 in Sveg
+        <br />
+        Raised to be a 'Härjedaling'
+      </p>
+    </div>
   );
 };
 
