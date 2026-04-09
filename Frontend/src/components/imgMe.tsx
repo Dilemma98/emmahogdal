52eefa22f85f42a13b39efd1a7e25c72399b234a
Emma Högdal <emmaah98@outlook.com>
2026-04-09 14:22:21 +0200

    new headshot

--- Frontend/src/components/imgMe.tsx
+++ Frontend/src/components/imgMe.tsx
@@ -1,9 +1,9 @@
-import imgUrl from "../assets/images/imgMe3.jpg";
+import imgUrl from "../assets/images/imgMe.jpg";
 import "../stylesheets/imgMe.css";
 
 const ImgMe = () => {
   return(
-    <div style={{marginTop: "5em"}}>
+    <div style={{marginTop: "6em"}}>
      <img className="imgMe" src={imgUrl} alt="Bild på Emma" />
      </div>
   );
