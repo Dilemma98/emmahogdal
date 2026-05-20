405f4127328c2ebc47cf6992cb9de0d1d0f1a0b8
Emma Högdal <emmaah98@outlook.com>
2026-05-20 13:28:45 +0200

    try fix pdf download

--- src/components/downloadCV.tsx
+++ src/components/downloadCV.tsx
@@ -4,7 +4,7 @@ import "../stylesheets/downloadCv.css";
 export default function DownloadCv(){
     return(
         <div className="downloadCvBtn">
-          <a href="data/cv.pdf" download="Emma_Högdal_CV.pdf" title="Download CV">
+          <a href="data/cv.pdf" download="cv.pdf" title="Download CV">
             <SimCardDownloadIcon fontSize="large" />
           </a>
         </div>
