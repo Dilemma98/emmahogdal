035d58a534084e095ec8d2f334aa3465acc73113
Emma Högdal <emmaah98@outlook.com>
2026-05-26 11:14:07 +0200

    font works live

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,12 +5,12 @@ const ContactMe = () => {
   return (
     <div className="contactMe">
       <h2>Let´s connect!</h2>
-      {/* <div className="links">
+      <div className="links">
                 <a href="mailto:emma.h98@outlook.com" ><i className="fa-solid fa-envelope"></i></a>
                 <a href="https://www.linkedin.com/in/emma-h%C3%B6gdal-07167b326/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
                 <a href="https://github.com/Dilemma98" target="_blank"><i className="fa-brands fa-github"></i></a>
-            </div> */}
-      <p>Har du någon fråga? Hör gärna av dig!</p>
+            </div>
+      {/* <p>Har du någon fråga? Hör gärna av dig!</p>
 
       <div className="fields">
         <div className="field">
@@ -50,7 +50,7 @@ const ContactMe = () => {
         <a href="https://github.com/Dilemma98" target="_blank">
           <i className="fa-brands fa-github"></i>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
